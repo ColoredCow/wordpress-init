b8bc859092262067b8bbfa3556a316e3cbc95d02
Vaibhav Rathore <vaibhav@coloredcow.com>
2017-03-08 14:51:20 +0530

    functions naming

--- public/wp-content/themes/ColoredCow/functions.php
+++ public/wp-content/themes/ColoredCow/functions.php
@@ -1,22 +1,22 @@
 <?php 
 
-if ( ! function_exists( 'colored_cow_scripts' ) ) {
-    function colored_cow_scripts() {
+if ( ! function_exists( 'cc_scripts' ) ) {
+    function cc_scripts() {
         wp_enqueue_script('cc-jquery', get_template_directory_uri().'/dist/lib/js/jquery-1.11.3.min.js');
         wp_enqueue_script('cc-bootstrap', get_template_directory_uri().'/dist/lib/js/bootstrap.min.js');
         wp_enqueue_script('main', get_template_directory_uri().'/main.js');
 
     }
-    add_action('wp_enqueue_scripts','colored_cow_scripts');
+    add_action('wp_enqueue_scripts','cc_scripts');
 }
 
-if ( ! function_exists( 'colored_cow_styles' ) ) {
-    function colored_cow_styles() {  
+if ( ! function_exists( 'cc_styles' ) ) {
+    function cc_styles() {  
         wp_enqueue_style('cc-bootstrap', get_template_directory_uri().'/dist/lib/css/bootstrap.min.css');
         wp_enqueue_style('style', get_template_directory_uri().'/style.css');
 
     }
-    add_action('wp_enqueue_scripts','colored_cow_styles');
+    add_action('wp_enqueue_scripts','cc_styles');
 }
 
 //add filter to remove margin above html
