7179bc3d7dbdcdc0d3a5b0861580de26aa260863
mohitgusain <mohit.gusain@coloredcow.com>
2020-04-05 14:43:10 +0530

    revert commit

---
+++ public/wp-content/themes/coloredcow/main.js
@@ -0,0 +1,2 @@
+/*! ColoredCow 2020-04-03 */
+jQuery(document).ready(function(){jQuery(".navbar-toggle").on("click",function(){jQuery(this).hasClass("navbar-toggle-cross")?(jQuery(".mobile-menu").removeClass("d-flex"),jQuery("html, body").removeClass("overflow-hidden position-relative h-100p")):(jQuery(".mobile-menu").addClass("d-flex"),jQuery("html, body").addClass("overflow-hidden position-relative h-100p")),jQuery(this).toggleClass("navbar-toggle-cross")})});
