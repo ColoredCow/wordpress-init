9b24c22f81fa8a862f9f30b1d401557bf046c47f
Pankaj Kandpal <pankaj.kandpal@coloredcow.in>
2021-06-26 20:39:48 +0530

    recent changes

--- public/wp-content/themes/coloredcow/main.js
+++ public/wp-content/themes/coloredcow/main.js
@@ -1,3 +1,3 @@
-/*! ColoredCow 2021-06-25 */
+/*! ColoredCow 2021-06-26 */
 
 jQuery(document).ready(function(){jQuery(".navbar-toggle").on("click",function(){jQuery(this).hasClass("navbar-toggle-cross")?(jQuery(".mobile-menu").removeClass("d-flex"),jQuery("html, body").removeClass("overflow-hidden position-relative h-100p")):(jQuery(".mobile-menu").addClass("d-flex"),jQuery("html, body").addClass("overflow-hidden position-relative h-100p")),jQuery(this).toggleClass("navbar-toggle-cross")})});
